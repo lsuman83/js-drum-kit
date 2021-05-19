1c23e2073566cb3f132787952501a2a45b93ce12
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-19 12:36:52 -0400

    finalizing functionality

--- listeners.js
+++ listeners.js
@@ -1,12 +1,23 @@
-//window.addEventListener('keydown', playSound)
 
-//function playSound (e){
- //   console.log(e);
-  //  const sound = document.querySelector(`audio[data-key="${e.keyCode}"`)
-   // const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+function playSound (e){
+   
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+   console.log(sound)
+    if(!sound) return;
 
-  //  if(!sound) return;
+    key.classList.add("playing");
+    sound.currentTime = 0;
+    sound.play()
+}
 
-   // sound.currentTime = 0;
-   // sound.play()
-//}
+function removeSound (e){
+   if(e.propertyName !== "transform") return;
+
+   e.target.classList.remove("playing");
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach((key) => key.addEventListener("transitionend", removeSound))
+
+document.addEventListener('keydown', playSound)
