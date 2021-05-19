c3efd9f0d0804b997055a0988e9a2641e175dde8
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-05-19 10:05:22 -0400

    wip

--- listeners.js
+++ listeners.js
@@ -1,7 +1,12 @@
-document.addEventListener('keydown', playSound)
+//window.addEventListener('keydown', playSound)
 
-function playSound (e){
-    debugger
-    const sound = document.querySelector(`audio[data-key="${e.keyCode}"`)
-    sound.play()
-}
+//function playSound (e){
+ //   console.log(e);
+  //  const sound = document.querySelector(`audio[data-key="${e.keyCode}"`)
+   // const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+
+  //  if(!sound) return;
+
+   // sound.currentTime = 0;
+   // sound.play()
+//}
